6acdd01fb2f9b11758400291c0eeac98ccd00a6e
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-22 03:57:07 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -6,44 +6,6 @@ export const themes: ThemeOption[] = [
   { value: "system", label: "System", icon: "/assets/icons/computer.svg" },
 ];
 
-export const sidebarLinks: SidebarLink[] = [
-  {
-    imgURL: "/assets/icons/home.svg",
-    route: "/",
-    label: "Home",
-  },
-  {
-    imgURL: "/assets/icons/users.svg",
-    route: "/community",
-    label: "Community",
-  },
-  {
-    imgURL: "/assets/icons/star.svg",
-    route: "/collection",
-    label: "Collections",
-  },
-  {
-    imgURL: "/assets/icons/suitcase.svg",
-    route: "/jobs",
-    label: "Find Jobs",
-  },
-  {
-    imgURL: "/assets/icons/tag.svg",
-    route: "/tags",
-    label: "Tags",
-  },
-  {
-    imgURL: "/assets/icons/user.svg",
-    route: "/profile",
-    label: "Profile",
-  },
-  {
-    imgURL: "/assets/icons/question.svg",
-    route: "/ask-question",
-    label: "Ask a question",
-  },
-];
-
 export const BADGE_CRITERIA = {
   QUESTION_COUNT: {
     BRONZE: 10,
